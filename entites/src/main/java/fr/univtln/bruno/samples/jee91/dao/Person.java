8ef2b732047d670db5b4ee2412cb060b32081e1f
Emmanuel Bruno <emmanuel.bruno@univ-tln.fr>
2021-11-29 16:46:05 +0100

    improves H2 docker images, URL, port, Psql compatibility.

--- entites/src/main/java/fr/univtln/bruno/samples/jee91/dao/Person.java
+++ entites/src/main/java/fr/univtln/bruno/samples/jee91/dao/Person.java
@@ -1,6 +1,7 @@
 package fr.univtln.bruno.samples.jee91.dao;
 
 import jakarta.persistence.*;
+import jakarta.xml.bind.annotation.XmlRootElement;
 
 @Entity
 @NamedQueries({
@@ -10,6 +11,7 @@ import jakarta.persistence.*;
                 name = "Person.findAllOrderedByName",
                 query = "SELECT p FROM Person p ORDER BY p.name")
 })
+@XmlRootElement
 public class Person {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
---
+++ restApp/src/main/resources/insert.sql
@@ -0,0 +1,4 @@
+INSERT INTO "public"."person" ("name") VALUES ('pierre');
+INSERT INTO "public"."person" ("name") VALUES ('Marie');
+INSERT INTO "public"."person" ("name") VALUES ('jacques');
+INSERT INTO "public"."person" ("name") VALUES ('Annie');
